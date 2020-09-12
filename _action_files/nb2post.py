5706071e7c9552c4677ec0a35666a5c59b6f489a
CHLoschen <chrissly31415@gmail.com>
2020-09-12 10:09:26 +0200

    another /sync

--- _action_files/nb2post.py
+++ _action_files/nb2post.py
@@ -2,11 +2,15 @@
 from datetime import datetime
 import re, os, logging
 from nbdev import export2html
-from nbdev.export2html import Config, Path, _re_digits
+from nbdev.export2html import Config, Path, 
 from fast_template import rename_for_jekyll
 
 warnings = set()
-    
+
+
+_re_digits = re.compile(r'^\d+_')
+
+
 # Modify the naming process such that destination files get named properly for Jekyll _posts
 def _nb2htmlfname(nb_path, dest=None): 
     fname = rename_for_jekyll(nb_path, warnings=warnings)
